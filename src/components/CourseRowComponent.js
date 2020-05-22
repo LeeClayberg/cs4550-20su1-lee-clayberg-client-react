c5d93bbb5c8b29f7de4e83d43cf19d9fb4a4a01a
Lee Clayberg <lee.clayberg@gmail.com>
2020-05-22 16:11:01 -0400

    Added Ok button and fixed formtting on text field

--- src/components/CourseRowComponent.js
+++ src/components/CourseRowComponent.js
@@ -29,20 +29,26 @@ export default class CourseRowComponent extends React.Component {
     return(
         <tr className="wbdv-row wbdv-course">
             <td className="large-cell align-middle text-truncate">
-                <i className="fa fa-file-text doc-icon wbdv-row wbdv-icon"></i>
-                {
-                    !this.state.editing &&
-                    <Link to={`/editor/${this.state.course._id}`}>
-                        {this.state.course.title}
-                    </Link>
-                }
-                {
-                    this.state.editing &&
-                    <input
-                        className="form-control"
-                        onChange={(event) => this.updateCourseTitle(event.target.value)}
-                        value={this.state.course.title}/>
-                }
+                <div className="form-group row">
+                    <div className="col-xs-1">
+                        <i className="fa fa-file-text doc-icon wbdv-row wbdv-icon align-middle"></i>
+                    </div>
+                    {
+                        !this.state.editing &&
+                            <Link className="align-middle title-text" to={`/editor/${this.state.course._id}`}>
+                                {this.state.course.title}
+                            </Link>
+                    }
+                    {
+                        this.state.editing &&
+                        <div className="col-xs-11">
+                            <input
+                                className="form-control align-middle"
+                                onChange={(event) => this.updateCourseTitle(event.target.value)}
+                                value={this.state.course.title}/>
+                        </div>
+                    }
+                </div>
             </td>
             <td className="d-none d-md-table-cell small-cell align-middle row-light-text wbdv-row wbdv-owner">
                 {this.state.course.owner}
@@ -55,12 +61,15 @@ export default class CourseRowComponent extends React.Component {
                     !this.state.editing &&
                     <span className="float-right">
                         <i className="fa fa-pencil fa-lg row-button-spacing"
-                           onClick={() => this.setEditing(true)}>
-                        </i>
+                           onClick={() => this.setEditing(true)}/>
                         <i className="fa fa-trash fa-lg row-button-spacing"
-                            onClick={() => this.props.deleteCourse(this.props.course)}>
-                        </i>
+                            onClick={() => this.props.deleteCourse(this.props.course)}/>
                     </span>
+                }
+                {
+                    this.state.editing &&
+                    <i className="fa fa-check fa-lg row-button-spacing float-right"
+                        onClick={this.ok}/>
                 }
             </td>
         </tr>
