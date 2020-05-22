62711adf8b9f39effce8b12dbeee91ea2835c00b
Lee Clayberg <lee.clayberg@gmail.com>
2020-05-22 19:56:06 -0400

    Added header for grid

--- src/components/CourseGridComponent.js
+++ src/components/CourseGridComponent.js
@@ -1,11 +1,39 @@
 import React from "react";
+import CourseRowComponent from "./CourseRowComponent";
 
 export default class CourseGridComponent
   extends React.Component
 {
   render() {
     return(
-      <h3>Course Grid {this.props.courses.length}</h3>
+        <div>
+          <table className="table">
+            <thead>
+            <tr>
+              <th className="d-table-cell large-cell header-height align-middle dark-gray wbdv-header wbdv-title">
+                Recent Documents
+              </th>
+              <th className="d-none d-md-table-cell small-cell header-height align-middle dark-gray wbdv-header wbdv-owner">
+                Owned by me
+                <i className="fa fa-caret-down drop-down-arrow"></i>
+              </th>
+              <th className="d-none d-md-table-cell small-cell header-height align-middle last-modified-size
+              dark-gray wbdv-header wbdv-last-modified"/>
+              <th className="small-cell header-height align-middle dark-gray"
+                  align="center">
+                <div>
+                  <i className="fa fa-th-list row-button-spacing"
+                     onClick={() =>
+                         this.props.setLayout('grid')}/>
+                  <a className="wbdv-header wbdv-sort">
+                    <i className="fa fa-sort-alpha-asc row-button-spacing"></i>
+                  </a>
+                </div>
+              </th>
+            </tr>
+            </thead>
+          </table>
+        </div>
     )
   }
 }
