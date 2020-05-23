128326095a46e0fa54c71653c796c08461ad9654
Lee Clayberg <lee.clayberg@gmail.com>
2020-05-23 15:03:11 -0400

    Made Module List Component

--- src/components/CourseEditor.js
+++ src/components/CourseEditor.js
@@ -56,60 +56,9 @@ class CourseEditor
                     </div>
                 </div>
                 <div className="row no-margin">
-                    <div className="col-md-4 no-padding">
-                        <div className="module-list">
-                            <ul className="list-group wbdv-module-list">
-                                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
-                                    <a className="wbdv-module-item-title">
-                                        Module 1 - jQuery
-                                    </a>
-                                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
-                                </li>
-                                <li className="list-group-item active align-middle wbdv-module-item">
-                                    <a className="wbdv-module-item-title">
-                                        Module 2 - React
-                                    </a>
-                                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
-                                    <a className="d-md-none float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">
-                                        <i className="fa fa-caret-down fa-lg"></i>
-                                    </a>
-                                </li>
-                                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
-                                    <a className="wbdv-module-item-title">
-                                        Module 3 - Redux
-                                    </a>
-                                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
-                                </li>
-                                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
-                                    <a className="wbdv-module-item-title">
-                                        Module 4 - Native
-                                    </a>
-                                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
-                                </li>
-                                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
-                                    <a className="wbdv-module-item-title">
-                                        Module 5 - Angular
-                                    </a>
-                                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
-                                </li>
-                                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
-                                    <a className="wbdv-module-item-title">
-                                        Module 6 - Node
-                                    </a>
-                                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
-                                </li>
-                                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
-                                    <a className="wbdv-module-item-title">
-                                        Module 7 - Mongo
-                                    </a>
-                                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
-                                </li>
-                            </ul>
-                            <div className="align-middle" align="right">
-                                <i className="d-none d-md-block fa fa-plus fa-lg bottom-plus wbdv-module-item-add-btn"></i>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        <ModuleList/>
+                    }
                     <div className="col-md-8 no-padding">
                         <div className="topics">
                             <ul className="nav nav-pills nav-fill wbdv-topic-pill-list">
--- src/components/ModuleList.js
+++ src/components/ModuleList.js
@@ -1,14 +1,59 @@
 import React from "react";
 
 const ModuleList = () =>
-  <div>
-    <h3>Module List</h3>
-    <ul>
-      <li>Module A</li>
-      <li>Module B</li>
-      <li>Module C</li>
-      <li>Module D</li>
-    </ul>
-  </div>
+    <div className="col-md-4 no-padding">
+        <div className="module-list">
+            <ul className="list-group wbdv-module-list">
+                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
+                    <a className="wbdv-module-item-title">
+                        Module 1 - jQuery
+                    </a>
+                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                </li>
+                <li className="list-group-item active align-middle wbdv-module-item">
+                    <a className="wbdv-module-item-title">
+                        Module 2 - React
+                    </a>
+                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                    <a className="d-md-none float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">
+                        <i className="fa fa-caret-down fa-lg"></i>
+                    </a>
+                </li>
+                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
+                    <a className="wbdv-module-item-title">
+                        Module 3 - Redux
+                    </a>
+                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                </li>
+                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
+                    <a className="wbdv-module-item-title">
+                        Module 4 - Native
+                    </a>
+                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                </li>
+                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
+                    <a className="wbdv-module-item-title">
+                        Module 5 - Angular
+                    </a>
+                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                </li>
+                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
+                    <a className="wbdv-module-item-title">
+                        Module 6 - Node
+                    </a>
+                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                </li>
+                <li className="d-none d-md-block list-group-item align-middle wbdv-module-item">
+                    <a className="wbdv-module-item-title">
+                        Module 7 - Mongo
+                    </a>
+                    <a className="d-none d-lg-block float-right btn x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                </li>
+            </ul>
+            <div className="align-middle" align="right">
+                <i className="d-none d-md-block fa fa-plus fa-lg bottom-plus wbdv-module-item-add-btn"></i>
+            </div>
+        </div>
+    </div>
 
 export default ModuleList
