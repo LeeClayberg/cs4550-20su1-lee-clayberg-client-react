c82f01c25f40905fab72c17f0dc8e438c753632a
Lee Clayberg <lee.clayberg@gmail.com>
2020-05-23 15:36:54 -0400

    Reformatted Home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,24 +2,34 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const HomeComponent = () =>
-  <div>
-    <h2>Home</h2>
-    <div className="list-group">
-      <Link className="list-group-item" to='/editor'>
-        Course Editor
-      </Link>
-      <Link className="list-group-item" to='/table/courses'>
-        Course List
-      </Link>
-      <Link className="list-group-item" to='/login'>
-        Login
-      </Link>
-      <Link className="list-group-item" to='/register'>
-        Registration
-      </Link>
-      <Link className="list-group-item" to='/profile'>
-        Profile
-      </Link>
+  <div className="container">
+    <h2 className="home-title">Home</h2>
+    <div>
+      <div>
+        <Link className="home-links" to='/editor'>
+          Course Editor
+        </Link>
+      </div>
+      <div>
+        <Link className="home-links" to='/table/courses'>
+          Course List
+        </Link>
+      </div>
+      <div>
+        <Link className="home-links" to='/login'>
+          Login
+        </Link>
+      </div>
+      <div>
+        <Link className="home-links" to='/register'>
+          Registration
+        </Link>
+      </div>
+      <div>
+        <Link className="home-links" to='/profile'>
+          Profile
+        </Link>
+      </div>
     </div>
   </div>
 
