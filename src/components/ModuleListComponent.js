274b94b0017dba3aea8e9c7ca00018d9a824c498
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-06 15:55:13 -0400

    Implemented editing

--- src/components/ModuleListComponent.js
+++ src/components/ModuleListComponent.js
@@ -7,7 +7,6 @@ class ModuleListComponent extends React.Component {
     }
 
     componentDidMount() {
-        // this.props.findAllModules()
         this.props.findModuleForCourse(this.props.params.courseId)
     }
 
@@ -19,10 +18,38 @@ class ModuleListComponent extends React.Component {
                         {
                         this.props.modules.map(module =>
                             <li key={module._id} className="list-group-item align-middle wbdv-module-item">
-                                <a className="wbdv-module-item-title">
-                                    {module.title}
-                                </a>
-                                <a className="float-right btn wbdv-x-button font-weight-bold wbdv-module-item-delete-btn">X</a>
+                                {
+                                    this.state.editingModule._id === module._id &&
+                                    <span>
+                                        <input value={this.state.editingModule.title}
+                                                onChange={(e) => {
+                                                    const newTitle = e.target.value
+                                                    this.setState(prevState => ({
+                                                        editingModule: {
+                                                        ...prevState.editingModule,
+                                                        title: newTitle
+                                                    }
+                                                }))
+                                            }}/>
+                                        <i className="btn fa fa-times float-right wbdv-module-button wbdv-module-item-delete-btn"
+                                            onClick={() => this.props.deleteModule(module._id)}/>
+                                        <i className="btn fa fa-check float-right wbdv-module-button wbdv-module-item-save-btn"
+                                           onClick={() => {
+                                               this.props.updateModule(this.state.editingModule._id, this.state.editingModule)
+                                               this.setState({editingModule: {}})
+                                           }}/>
+                                    </span>
+                                }
+                                {
+                                    this.state.editingModule._id !== module._id &&
+                                    <span>
+                                        <a className="wbdv-module-item-title">
+                                            {module.title}
+                                        </a>
+                                        <i className="btn fa fa-pencil float-right wbdv-module-button wbdv-module-item-edit-btn"
+                                            onClick={() => this.setState({editingModule: module})}/>
+                                    </span>
+                                }
                             </li>
                         )}
                     </ul>
