c7606ac27f6027b762f14c600913e3212e257fbc
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-06 14:32:12 -0400

    Swicthed name on generic server, and added he starter modeule container

--- src/services/ModuleService.js
+++ src/services/ModuleService.js
@@ -1,22 +1,22 @@
 const findAllModules = () => {
-  return fetch("https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules")
+  return fetch("https://wbdv-generic-server.herokuapp.com/api/001458299/modules")
     .then(response => response.json())
 }
 
 const findModuleForCourse = (courseId) =>
-  fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/courses/${courseId}/modules`)
+  fetch(`https://wbdv-generic-server.herokuapp.com/api/001458299/courses/${courseId}/modules`)
     .then(response => response.json())
 
 
 const deleteModule = (moduleId) => {
-  return fetch("https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules/" + moduleId, {
+  return fetch("https://wbdv-generic-server.herokuapp.com/api/001458299/modules/" + moduleId, {
     method: 'DELETE'
   })
     .then(response => response.json())
 }
 
 const updateModule = (moduleId, newModule) =>
-  fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules/${moduleId}`, {
+  fetch(`https://wbdv-generic-server.herokuapp.com/api/001458299/modules/${moduleId}`, {
     method: 'PUT',
     body: JSON.stringify(newModule),
     headers: {
