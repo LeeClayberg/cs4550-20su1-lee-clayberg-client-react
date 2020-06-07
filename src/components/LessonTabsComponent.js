5aea31d35bc8c3f5a0cc6d61b9ece00eb3a3abd2
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-06 22:55:32 -0400

    Added update and reformated the buttons

--- src/components/LessonTabsComponent.js
+++ src/components/LessonTabsComponent.js
@@ -1,46 +1,90 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 class LessonTabsComponent extends React.Component {
-        state = {
-                newLessonTitle: 'New Lesson',
-                editingLesson: {},
-                selected: {}
-        }
+    state = {
+        newLessonTitle: 'New Lesson',
+        editingLesson: {},
+        selected: {}
+    }
 
-        componentDidMount() {
+    componentDidMount() {
+        this.props.findLessonsForModule(this.props.params.moduleId)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.params.moduleId !== this.props.params.moduleId) {
             this.props.findLessonsForModule(this.props.params.moduleId)
         }
+    }
 
-        componentDidUpdate(prevProps, prevState, snapshot) {
-                if(prevProps.params.moduleId !== this.props.params.moduleId) {
-                        this.props.findLessonsForModule(this.props.params.moduleId)
-                }
-        }
+    render() {
+        return (
+            <span>
+                <ul className="nav nav-pills nav-fill wbdv-lesson-tabs float-right">
+                     {
+                         this.props.lessons.map(lesson =>
+                              <Link className={`nav-item nav-link ${this.state.selected === lesson._id ? "active" : ""}`}
+                                  onClick={() => {
+                                          this.setState({
+                                                                selected: lesson._id
+                                                        });
+                                  }}>
+                                      {
+                                              this.state.editingLesson._id === lesson._id &&
+                                              <input
+                                                  value={this.state.editingLesson.title}
+                                                  onChange={(e) => {
+                                                          const newTitle = e.target.value
+                                                          this.setState(
+                                                              prevState => ({
+                                                                      editingLesson: {
+                                                                              ...prevState.editingLesson,
+                                                                              title: newTitle
 
-        render() {
-                return (
-                    <span>
-                        <ul className="nav nav-pills nav-fill wbdv-lesson-tabs float-right">
-                            {
-                                this.props.lessons.map(lesson =>
-                                     <il className="nav-item nav-link">
-                                             {lesson.title}
-                                             <i className="btn fa fa-times float-right wbdv-module-button wbdv-module-item-delete-btn"
+                                                                      }
+                                                              }))
+                                                  }}/>
+                                      }
+                                      {
+                                              this.state.editingLesson._id !== lesson._id &&
+                                              <span className="wbdv-module-item-title">
+                                                        {lesson.title}
+                                              </span>
+                                      }
+                                      {
+                                              this.state.editingLesson._id === lesson._id &&
+                                              <span>
+                                                <i className="btn fa fa-times float-right wbdv-lesson-button"
                                                 onClick={() => this.props.deleteLesson(lesson._id)}/>
-                                     </il>)
-                            }
-                            <il className="wbdv-no-padding">
-                                <i className="btn fa fa-plus fa-lg wbdv-top-plus wbdv-lesson-add-btn float-right"
-                                   onClick={() => this.props.createLesson(
-                                       this.props.params.moduleId,
-                                       {
-                                               title: this.state.newLessonTitle
-                                       })}/>
-                            </il>
-                        </ul>
-                    </span>
-                )
-        }
+                                                <i className="btn fa fa-check float-right wbdv-lesson-button"
+                                                onClick={() => {
+                                                   this.props.updateLesson(this.state.editingLesson._id, this.state.editingLesson)
+                                                   this.setState({editingLesson: {}})}}/>
+                                              </span>
+                                      }
+                                      {
+                                              this.state.editingLesson._id !== lesson._id && this.state.selected === lesson._id &&
+                                              <i className="btn fa fa-pencil float-right wbdv-lesson-button"
+                                                 onClick={() => this.setState({editingLesson: lesson})}/>
+                                      }
+                              </Link>)
+                     }
+                     {
+                             this.props.params.moduleId &&
+                             <il className="wbdv-no-padding">
+                                     <i className="btn fa fa-plus fa-lg wbdv-top-plus wbdv-lesson-add-btn float-right"
+                                        onClick={() => this.props.createLesson(
+                                            this.props.params.moduleId,
+                                            {
+                                                    title: this.state.newLessonTitle
+                                            })}/>
+                             </il>
+                     }
+                </ul>
+            </span>
+        )
+    }
 }
 
 export default LessonTabsComponent
