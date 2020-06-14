f60e28cf8cef2bfd0b8ab4f890d2acc911cd2473
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-14 19:27:45 -0400

    Widget service

---
+++ src/services/WidgetService.js
@@ -0,0 +1,39 @@
+
+const findWidgetsForTopic = (lessonId) =>
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/topics/${lessonId}/widgets`)
+        .then(response => response.json())
+
+const deleteWidget = (widgetId) => {
+    return fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/widgets/${widgetId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+}
+
+const updateWidget = (widgetId, widget) =>
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/widgets/${widgetId}`, {
+        method: 'PUT',
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+const createWidget = (topicId, newWidget) =>
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/topics/${topicId}/widgets`, {
+        method: 'POST',
+        body: JSON.stringify(newWidget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+const findAllWidgets = (lessonId) =>
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/topics/${lessonId}/widgets`)
+        .then(response => response.json())
+
+export default {
+
+}
