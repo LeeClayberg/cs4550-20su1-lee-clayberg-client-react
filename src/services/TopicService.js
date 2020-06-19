64e9c5c9e373654a21a9aa1412aca27bbf3aa0b8
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-19 17:38:37 -0400

    Topic service updates

--- src/services/TopicService.js
+++ src/services/TopicService.js
@@ -1,16 +1,16 @@
 const findTopicsForLesson = (lessonId) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/001458299/lessons/${lessonId}/topics`)
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/lessons/${lessonId}/topics`)
         .then(response => response.json())
 
 const deleteTopic = (topicId) => {
-    return fetch(`https://wbdv-generic-server.herokuapp.com/api/001458299/topics/${topicId}`, {
+    return fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/topics/${topicId}`, {
         method: 'DELETE'
     })
         .then(response => response.json())
 }
 
 const updateTopic = (topicId, topic) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/001458299/topics/${topicId}`, {
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/topics/${topicId}`, {
         method: 'PUT',
         body: JSON.stringify(topic),
         headers: {
@@ -20,13 +20,17 @@ const updateTopic = (topicId, topic) =>
         .then(response => response.json())
 
 const createTopic = (lessonId, newTopic) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/001458299/lessons/${lessonId}/topics`, {
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/lessons/${lessonId}/topics`, {
         method: 'POST',
         body: JSON.stringify(newTopic),
         headers: {
             'content-type': 'application/json'
         }
     })
+        .then(response => response.json())
+
+const findTopicById = (topicId) =>
+    fetch(`https://cs4550-20sm1-clayberg-server.herokuapp.com/api/topics/${topicId}`)
         .then(response => response.json())
 
 export default {
